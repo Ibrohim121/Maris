bfc71ed00c011553b3e7d185828f00e1111bf71a
Ibrohim <igofurjonov229@gmail.com>
2026-07-05 18:42:38 +0500

    fix: remove black overlay screenshot protection

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Providers from "@/components/Providers";
 import Navbar from "@/components/Navbar";
-import ScreenshotProtection from "@/components/ScreenshotProtection";
+
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -34,9 +34,7 @@ export default function RootLayout({
       <body className="min-h-full flex flex-col">
         <Providers>
           <Navbar />
-          <ScreenshotProtection>
-            {children}
-          </ScreenshotProtection>
+          {children}
         </Providers>
       </body>
     </html>
